8e44bdb1e5a46fc15fdeef2b6e14e7a2d7e0a7f5
Ubuntu <ubuntu@instance-1.asia-northeast1-b.c.kaggle-zzz.internal>
2023-10-23 13:59:55 +0000

    update config format

---
+++ src/conf/config.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Any
+
+
+@dataclass
+class ExperimentConfig:
+    wandb: bool = False
+    project: str = "kaggle-zzz"
+    group: str = "hoge"
+
+
+@dataclass
+class LSTMConfig:
+    hidden_dim: int = 8
+    num_layers: int = 1
+
+
+@dataclass
+class NNTrainConfig:
+    batch_size: int = 32
+    epochs: int = 3
+    learning_rate: float = 0.01
+    patience: int = 10
+
+
+@dataclass
+class Config:
+    hydra: Any = field(
+        default_factory=lambda: {"run": {"dir": f"./outputs/{ExperimentConfig.group}"}}
+    )
+    experiment: ExperimentConfig = ExperimentConfig()
+    series_data_path: Path = Path("~/kaggle-zzz/data/small_data/Zzzs_train.parquet")
+    events_data_path: Path = Path("~/kaggle-zzz/data/raw_data/train_events.csv")
+    features: list[str] = field(default_factory=lambda: ["enmo", "anglez"])
+    targets: list[str] = field(
+        default_factory=lambda: ["onset_heatmap", "wakeup_heatmap"]
+    )
+    seed: int = 42
+    n_splits: int = 4
+    model: LSTMConfig = LSTMConfig()
+    train: NNTrainConfig = NNTrainConfig()
--- src/train.py
+++ src/train.py
@@ -3,6 +3,8 @@ import gc
 import hydra
 import polars as pl
 import torch
+from hydra.core.config_store import ConfigStore
+from omegaconf import OmegaConf
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
@@ -10,18 +12,19 @@ from sklearn.model_selection import KFold
 from torch.utils.data import DataLoader
 
 import wandb
+from conf.config import Config
 from model.lstm import LSTM, RNNModule
 from preprocessing.features import create_dataset, load_events_data, load_series_data
 from preprocessing.torch_dataset import SleepDataset, pad_sequence_fn
 from utils import fix_seed, seed_worker
 
+cs = ConfigStore.instance()
+cs.store(name="config", node=Config)
 
-@hydra.main(config_path="conf", config_name="config")
-def main(config):
-    params = config.params
-    features = config.features
-    targets = config.targets
-    fix_seed(params.seed)
+
+@hydra.main(version_base="1.1", config_name="config")
+def main(config: Config) -> None:
+    fix_seed(config.seed)
 
     # preprocessing
     df_series = load_series_data(config.series_data_path)
@@ -33,15 +36,15 @@ def main(config):
 
     # run cross validation
     series_id_list = df["series_id"].unique().to_numpy()
-    kfold = KFold(n_splits=params.n_splits, shuffle=True, random_state=params.seed)
+    kfold = KFold(n_splits=config.n_splits, shuffle=True, random_state=config.seed)
 
     for fold, (idx_tr, idx_va) in enumerate(kfold.split(series_id_list), start=1):
-        if config.wandb:
+        if config.experiment.wandb:
             wandb.init(
-                project=config.project,
-                group=config.group,
+                project=config.experiment.project,
+                group=config.experiment.group,
                 name=f"fold-{fold}",
-                config=dict(params),
+                config=dict(OmegaConf.to_container(config)),
             )
 
         # dataloader
@@ -49,49 +52,50 @@ def main(config):
         df_va = df.filter(pl.col("series_id").is_in(list(series_id_list[idx_va])))
 
         g = torch.Generator()
-        g.manual_seed(params.seed)
+        g.manual_seed(config.seed)
 
         loader_tr = DataLoader(
-            dataset=SleepDataset(df_tr, features, targets),
-            batch_size=params.batch_size,
+            dataset=SleepDataset(df_tr, config.features, config.targets),
+            batch_size=config.train.batch_size,
             shuffle=True,
             collate_fn=pad_sequence_fn,
             worker_init_fn=seed_worker,
             generator=g,
         )
         loader_va = DataLoader(
-            dataset=SleepDataset(df_va, features, targets),
-            batch_size=params.batch_size,
+            dataset=SleepDataset(df_va, config.features, config.targets),
+            batch_size=config.train.batch_size,
             collate_fn=pad_sequence_fn,
             worker_init_fn=seed_worker,
             generator=g,
         )
 
-        # training
+        # model
         lstm = LSTM(
-            input_dim=len(features),
-            hidden_dim=params.hidden_dim,
-            output_dim=len(targets),
-            num_layers=params.num_layers,
+            input_dim=len(config.features),
+            hidden_dim=config.model.hidden_dim,
+            output_dim=len(config.targets),
+            num_layers=config.model.num_layers,
         )
-        model = RNNModule(model=lstm, lr=params.learning_rate)
+        model = RNNModule(model=lstm, lr=config.train.learning_rate)
 
+        # training
         early_stopping_callback = EarlyStopping(
-            monitor="val_loss", patience=params.patience, verbose=False
+            monitor="val_loss", patience=config.train.patience, verbose=False
         )
         checkpoint_callback = ModelCheckpoint(
             monitor="val_loss", dirpath=".", filename=f"checkpoint-fold{fold}"
         )
-        wandb_logger = WandbLogger() if config.wandb else None
+        wandb_logger = WandbLogger() if config.experiment.wandb else None
 
         trainer = Trainer(
-            max_epochs=params.epochs,
+            max_epochs=config.train.epochs,
             callbacks=[early_stopping_callback, checkpoint_callback],
             logger=wandb_logger,
         )
         trainer.fit(model, loader_tr, loader_va)
 
-        if config.wandb:
+        if config.experiment.wandb:
             wandb.finish()
 
 
