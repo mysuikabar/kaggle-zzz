ea83fb5bfe510390cac0e6f2f30907a057883c32
mizuho <mizuho.suikabar@gmail.com>
2023-11-03 09:32:12 +0000

    update infer.py

--- src/infer.py
+++ src/infer.py
@@ -1,9 +1,9 @@
-import pandas as pd
 from omegaconf import OmegaConf
 
 from conf.config import InferConfig
-from model.lstm import LSTM, RNNModule
+from model.lstm import LSTM, RNNModule, predict
 from processing.features import load_series_data
+from processing.post_processing import make_submission
 from processing.torch_dataset import SleepDataset
 
 
@@ -13,7 +13,7 @@ def main():
 
     # load test data
     df_series = load_series_data(InferConfig.test_data_path)
-    dataset = SleepDataset(df_series, features=config.features)
+    dataset = SleepDataset(df=df_series, features=config.features)
 
     # load trained model
     lstm = LSTM(
@@ -30,22 +30,8 @@ def main():
     model.eval()
 
     # make submission.csv
-    submission = pd.DataFrame(columns=["series_id", "step", "event", "score"])
-
-    for i, X in enumerate(dataset):
-        pred, _ = model(X)
-        df_sub = dataset.data[i].to_pandas()
-        df_sub[["onset", "wakeup"]] = pred.detach().numpy()
-
-        for target in ["onset", "wakeup"]:
-            best_score_row = df_sub.loc[
-                df_sub[target].idxmax(),
-                ["series_id", "step", target],  # extract the row with the highest score
-            ].rename({target: "score"})
-            best_score_row["event"] = target
-            submission = pd.concat([submission, best_score_row.to_frame().T])
-
-    submission["row_id"] = list(range(len(submission)))
+    df_pred = predict(model=model, dataset=dataset)
+    submission = make_submission(df_pred)
     submission.to_csv(InferConfig.output_dir / "submission.csv", index=False)
 
 
